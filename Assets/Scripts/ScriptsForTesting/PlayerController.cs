f43cd12769b90b3cbf46e504d61315f2fd0b025d
Daniel <taddel1212@gmail.com>
2022-06-02 18:57:41 +0200

    Player has learned to fly

--- Assets/Scripts/ScriptsForTesting/PlayerController.cs
+++ Assets/Scripts/ScriptsForTesting/PlayerController.cs
@@ -7,6 +7,8 @@ using Valve.VR.InteractionSystem;
 public class PlayerController : MonoBehaviour
 {
     public SteamVR_Action_Vector2 input;
+    public Quaternion rot;
+    public HandPhysics handPhysics;
     public float speed = 1;
     private CharacterController characterController;
 
@@ -17,11 +19,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+             
         if(input.axis.magnitude > 0.1f){
-            Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
-            characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up)-new Vector3(0,9.81f,0)*Time.deltaTime); 
+            Vector3 direction = handPhysics.handCollider.transform.TransformDirection(input.axis.x,0,input.axis.y);
+            characterController.Move(speed * Time.deltaTime * direction); 
         }
         
-
     }
 }
