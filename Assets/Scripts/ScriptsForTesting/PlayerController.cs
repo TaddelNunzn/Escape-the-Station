71ec567ec06c25440139061f69c5e5b4443ce63c
Daniel <taddel1212@gmail.com>
2022-06-07 18:56:58 +0200

    They fly now

--- Assets/Scripts/ScriptsForTesting/PlayerController.cs
+++ Assets/Scripts/ScriptsForTesting/PlayerController.cs
@@ -1,29 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 using Valve.VR;
 using Valve.VR.InteractionSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public SteamVR_Action_Vector2 input;
-    public Quaternion rot;
+    public SteamVR_Action_Boolean stop;
     public HandPhysics handPhysics;
-    public float speed = 1;
+    [SerializeField] private float speed = 1;
     private CharacterController characterController;
+    public GameObject vrCamera;
+    Vector3 direction = Vector3.zero;
+
+    private Rigidbody rigidbody;
+    
+
 
     private void Start(){
         characterController = GetComponent<CharacterController>();
+        rigidbody = GetComponent<Rigidbody>();
+        
+
+        Valve.VR.OpenVR.Chaperone.ResetZeroPose(ETrackingUniverseOrigin.TrackingUniverseStanding);
+        PositionController();
+        
     }
 
     // Update is called once per frame
     void Update()
-    {
-             
-        if(input.axis.magnitude > 0.1f){
-            Vector3 direction = handPhysics.handCollider.transform.TransformDirection(input.axis.x,0,input.axis.y);
-            characterController.Move(speed * Time.deltaTime * direction); 
-        }
+    {   
+        
+
+        
+        
         
     }
+
+    void FixedUpdate(){
+        //PositionController();
+        MovePlayer();
+        /*if(input.axis.magnitude > 0.1f){
+            Vector3 direction = handPhysics.handCollider.transform.TransformDirection(input.axis.x,0,input.axis.y);
+            characterController.Move(speed * Time.fixedDeltaTime * direction); 
+        }*/
+    }
+
+
+    Vector3 currentSpeed;
+    float change = 0.25f;
+
+    bool pressed = false;
+
+    private void MovePlayer(){   
+        if(stop.state) rigidbody.AddForce(-rigidbody.velocity*10);
+        else if(input.axis.magnitude > 0.1f){
+            Vector3 direction = handPhysics.handCollider.transform.TransformDirection(input.axis.x,0,input.axis.y);
+            rigidbody.AddForce(direction);
+            
+        }
+        
+        characterController.Move(currentSpeed*Time.fixedDeltaTime*speed);        
+    }
+
+     //moves capsule with vr camera
+    private void PositionController()
+    {
+        Vector3 hmd = vrCamera.transform.position;
+        Vector3 rig = rigidbody.transform.position;
+        Vector3 diff = hmd - rig;
+        rigidbody.AddForce(diff.x,0,diff.y);
+    }
+    
 }
--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -113,6 +113,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean StopFlight
+        {
+            get
+            {
+                return SteamVR_Actions.default_StopFlight;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
