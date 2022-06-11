202cbffd0824c1bc690e1bc775e480b809010b6c
unknown <steffen.wolff1@gmx.net>
2022-06-11 20:50:09 +0200

    enhanced performance

--- Assets/Scripts/ChangeTexture.cs
+++ Assets/Scripts/ChangeTexture.cs
@@ -6,6 +6,7 @@ using UnityEngine.Events;
 public class ChangeTexture : MonoBehaviour
 {
 
+
     static bool doupdates = true;
     static bool finished = false;
     static int count = 0;
@@ -19,12 +20,10 @@ public class ChangeTexture : MonoBehaviour
     public Texture texturePressed5;
      public Texture finishtexture;
 
-    //public Material Material1;
 
     GameObject presser;
     public GameObject tochange;
 
-    // Start is called before the first frame update
     void Start()
     {
         m_renderer = tochange.GetComponent<Renderer> ();
@@ -35,7 +34,7 @@ public class ChangeTexture : MonoBehaviour
     m_renderer.material.SetTexture("_MainTex", finishtexture);
     finished = true;
     Debug.Log("i update");
-    Invoke("stopupdates", 0.1f);
+    Invoke("stopupdates", 0.01f);
     }
     }
 
@@ -53,13 +52,13 @@ void stopupdates(){
     doupdates = false;
     Debug.Log("i stopped");
 }
- public void ChangeT1(){
+ public void ChangeT1(int multi){
     if(count != 2140){
     switch(current){
         case 0:
          m_renderer.material.SetTexture("_MainTex", texturePressed);
          current++;
-         count +=10;
+         count +=multi;
          Debug.Log(count);
         break;
         
@@ -67,115 +66,32 @@ void stopupdates(){
         case 1:
          m_renderer.material.SetTexture("_MainTex", texturePressed2);
          current++;
-         count +=10;
+         count +=multi;
          Debug.Log(count);
         break;
 
         case 2:
         m_renderer.material.SetTexture("_MainTex", texturePressed3);
         current++;
-        count +=10;
+        count +=multi;
         Debug.Log(count);
         break;
 
         case 3:
         m_renderer.material.SetTexture("_MainTex", texturePressed4);
          current++;
-         count +=10;
+         count +=multi;
          Debug.Log(count);
         break;
 
         case 4:
         m_renderer.material.SetTexture("_MainTex", texturePressed5);
         current = 0;
-        count -=40;
+        count -= (4*multi);
         Debug.Log(count);
         break;
     }
     }
  }
 
-
- public void ChangeT2(){
-    if(count != 2140){
-    switch(current){
-        case 0:
-         m_renderer.material.SetTexture("_MainTex", texturePressed);
-         current++;
-         count +=100;
-         Debug.Log(count);
-        break;
-
-        case 1:
-         m_renderer.material.SetTexture("_MainTex", texturePressed2);
-         current++;
-         count +=100;
-         Debug.Log(count);
-        break;
-
-        case 2:
-        m_renderer.material.SetTexture("_MainTex", texturePressed3);
-        current++;
-        count +=100;
-        Debug.Log(count);
-        break;
-
-        case 3:
-        m_renderer.material.SetTexture("_MainTex", texturePressed4);
-         current++;
-         count +=100;
-         Debug.Log(count);
-        break;
-
-        case 4:
-        m_renderer.material.SetTexture("_MainTex", texturePressed5);
-        current = 0;
-        count -=400;
-        Debug.Log(count);
-        break;
-    }
-    }
- }
-
-
- public void ChangeT3(){
-    if(count != 2140){
-    switch(current){
-        case 0:
-         m_renderer.material.SetTexture("_MainTex", texturePressed);
-         current++;
-         count +=1000;
-         Debug.Log(count);
-        break;
-
-        case 1:
-         m_renderer.material.SetTexture("_MainTex", texturePressed2);
-         current++;
-         count +=1000;
-         Debug.Log(count);
-        break;
-
-        case 2:
-        m_renderer.material.SetTexture("_MainTex", texturePressed3);
-        current++;
-        count +=1000;
-        Debug.Log(count);
-        break;
-
-        case 3:
-        m_renderer.material.SetTexture("_MainTex", texturePressed4);
-         current++;
-         count +=1000;
-         Debug.Log(count);
-        break;
-
-        case 4:
-        m_renderer.material.SetTexture("_MainTex", texturePressed5);
-        current = 0;
-        count -=4000;
-        Debug.Log(count);
-        break;
-    }
-    }
- }
 }
