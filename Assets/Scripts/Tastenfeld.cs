54e145825b310f0edf8353afac9f510fa520e8a4
Daniel <taddel1212@gmail.com>
2022-06-12 19:30:50 +0200

    Work on Input Field

---
+++ Assets/Scripts/Tastenfeld.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tastenfeld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TestAktion(){
+        Debug.Log("Taste Gedrückt");    
+    }
+}
